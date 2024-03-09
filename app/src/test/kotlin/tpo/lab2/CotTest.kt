de9caf6b22a57a9c061fd2773a62c43e14b3da9d
Kirill Guryanov <korako2@mail.ru>
2024-03-09 03:09:57 +0300

    test: add cot test

--- app/src/test/kotlin/tpo/lab2/CotTest.kt
+++ app/src/test/kotlin/tpo/lab2/CotTest.kt
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import kotlin.math.PI
+import kotlin.math.sin
 import kotlin.math.tan
 
 class CotTest {
@@ -28,15 +30,56 @@ class CotTest {
         assertEquals(expected, actual.toDouble(), eps.toDouble())
     }
 
-    @DisplayName("cot(PI) and cot(0) is undefined")
+    @DisplayName("cot(PI + PIk) and cot(0) is undefined")
     @ParameterizedTest(name = "cot({0})")
-    @ValueSource(doubles = [0.0, Math.PI])
+    @ValueSource(doubles = [0.0, PI, -10000*PI])
     fun `test cot(PI) and cot(0)`(x: Double) {
         val cot = Cot()
         val eps = 1e-3.toBigDecimal()
         assertThrows(ArithmeticException::class.java) {
             cot.calc(x.toBigDecimal(), eps)
         }
+    }
+
+    @DisplayName("Invalid argument")
+    @ParameterizedTest(name = "cot({0})")
+    @ValueSource(doubles = [Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY])
+    fun `test invalid argument`(x: Double) {
+        val cot = Cot()
+        val eps = 1e-10.toBigDecimal()
+        assertThrows(IllegalArgumentException::class.java) {
+            cot.calc(x.toBigDecimal(), eps)
+        }
+    }
+
+    @ParameterizedTest(name = "cot({0})")
+    @ValueSource(doubles = [100.0, 1000.0, 10000.0])
+    fun `large argument numbers`(x : Double) {
+        val cot = Cot()
+        val eps = 1E-10.toBigDecimal()
+        val expected = 1/tan(x)
+        val actual = cot.calc(x.toBigDecimal(), eps).toDouble()
+        assertEquals(expected, actual, eps.toDouble())
+    }
+
+    @ParameterizedTest(name = "cot({0})")
+    @ValueSource(doubles = [PI/10, 2*PI/3, -2*PI/4, -1001*PI/12])
+    fun `frequency of the function`(x : Double) {
+        val cot = Cot()
+        val eps = 1E-6.toBigDecimal()
+        val expected = 1/tan(x)
+        val actual = cot.calc(x.toBigDecimal() + (PI).toBigDecimal(), eps).toDouble()
+        assertEquals(expected, actual, eps.toDouble())
+    }
+
+    @ParameterizedTest(name = "cot({0})")
+    @ValueSource(doubles = [PI/10, 2*PI/3, -2*PI/4, -1001*PI/12])
+    fun `parity of the function`(x : Double) {
+        val cot = Cot()
+        val eps = 1E-10.toBigDecimal()
+        val expected = -1/tan(x)
+        val actual = cot.calc(-x.toBigDecimal(), eps).toDouble()
+        assertEquals(expected, actual, eps.toDouble())
     }
 
 }
