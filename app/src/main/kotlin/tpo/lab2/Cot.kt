bfd040c5e0ecdc3d871e8832b079d09f5cb975cc
Kirill Guryanov <korako2@mail.ru>
2024-03-09 03:48:20 +0300

    fix: bug with vertical diameter points

--- app/src/main/kotlin/tpo/lab2/Cot.kt
+++ app/src/main/kotlin/tpo/lab2/Cot.kt
@@ -2,15 +2,22 @@ package tpo.lab2
 
 import java.math.BigDecimal
 import java.math.RoundingMode
+import kotlin.math.PI
 
 class Cot {
     private val tan = Tan()
 
     fun calc(x: BigDecimal, eps: BigDecimal): BigDecimal {
+        if (isPointsOfVerticalDiameter(x)) return 0.0.toBigDecimal()
         val tanValue = tan.calc(x, eps)
         if (tanValue.compareTo(BigDecimal.ZERO) == 0) {
             throw ArithmeticException("cot($x) is undefined")
         }
         return BigDecimal.ONE.divide(tanValue, eps.scale(), RoundingMode.HALF_EVEN)
+    }
+
+    private fun isPointsOfVerticalDiameter(x: BigDecimal): Boolean {
+        return (PI/2).toBigDecimal().subtract(x.abs().remainder(PI.toBigDecimal().multiply(2.toBigDecimal()))).compareTo((1E-10).toBigDecimal()) <= 0 ||
+                (3*PI/2).toBigDecimal().subtract(x.abs().remainder(PI.toBigDecimal().multiply(2.toBigDecimal()))).compareTo((1E-10).toBigDecimal()) <= 0
     }
 }
